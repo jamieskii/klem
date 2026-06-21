84e320c499abc150d056cd32519985c25608cf10
jamieskii <jamiekramer2000@gmail.com>
2026-06-21 11:54:11 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
-
 const menuToggle = document.querySelector(".menu-toggle");
 const nav = document.querySelector(".nav");
 
-menuToggle.addEventListener("click", () => {
+if (menuToggle && nav) {
+  menuToggle.addEventListener("click", () => {
     nav.classList.toggle("is-open");
 
     const isOpen = nav.classList.contains("is-open");
-    menuToggle.setAttribute("aria-label", isOpen ? "Menu sluiten" : "Menu openen");
-});
+    menuToggle.setAttribute(
+      "aria-label",
+      isOpen ? "Menu sluiten" : "Menu openen"
+    );
+  });
 
-document.querySelectorAll(".nav a").forEach((link) => {
+  document.querySelectorAll(".nav a").forEach((link) => {
     link.addEventListener("click", () => {
-        nav.classList.remove("is-open");
-        menuToggle.setAttribute("aria-label", "Menu openen");
+      nav.classList.remove("is-open");
+      menuToggle.setAttribute("aria-label", "Menu openen");
     });
-});
+  });
+}
